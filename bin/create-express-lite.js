735e0bc3f0e7cb5b87b704447f700c8fcd1b349e
MudassirQuraishi <mudassir.quraishi14@outlook.com>
2024-03-16 20:10:49 +0530

    minor changes done

--- bin/create-express-lite.js
+++ bin/create-express-lite.js
@@ -1,10 +1,8 @@
 #!/usr/bin/env node
-
 const util = require('util');
 const path = require('path');
 const fs = require('fs');
 const { execSync } = require('child_process');
-const cliProgress = require('cli-progress');
 
 const exec = util.promisify(require('child_process').exec);
 
@@ -32,31 +30,28 @@ async function setup() {
     console.log(
       'This utility will help you set up a new Express.js project quickly.'
     );
-
-    // Check if git is installed
     const hasGitInstalled = await hasGit();
     if (!hasGitInstalled) {
       throw new Error(
         'Git is not installed. Please install Git and try again.'
       );
     }
-
-    // Validate arguments
     if (process.argv.length < 3) {
       throw new Error(
         'Please specify the target project directory.\nFor example: npx create-express-lite my-app'
       );
     }
-
-    // Define constants
     const ownPath = process.cwd();
+    console.log(ownPath);
     const userProvidedName = process.argv[2];
-    const folderName =
-      userProvidedName === '.' ? path.basename(ownPath) : userProvidedName;
-    const appPath = path.join(ownPath, folderName);
+    const folderName = userProvidedName === '.' ? '' : userProvidedName;
+    let appPath;
+    if (folderName.length < 1) {
+      appPath = path.join(ownPath);
+    } else {
+      appPath = path.join(ownPath, folderName);
+    }
     const repo = 'https://github.com/the-developer-org/create-express-lite.git';
-
-    // Check if directory already exists
     try {
       fs.mkdirSync(appPath);
     } catch (err) {
@@ -68,49 +63,31 @@ async function setup() {
         throw err;
       }
     }
-
-    // Clone repo
-    console.log(`\nDownloading files from ${repo}`);
+    console.log(`\nSetting up your Express app.....`);
     await runCmd(`git clone --depth 1 ${repo} ${folderName}`);
-    console.log('Cloned successfully.\n');
-
-    // Change directory
+    console.log('Setup completed succesfully.\n');
     process.chdir(appPath);
-
-    // Install dependencies
     console.log('Installing dependencies...');
-    const progressBar = new cliProgress.SingleBar(
-      {},
-      cliProgress.Presets.shades_classic
-    );
-    progressBar.start(100, 0);
     const { stderr } = await runCmd('npm install');
-    progressBar.stop();
     if (stderr) {
       throw new Error(stderr);
     }
     console.log('Dependencies installed successfully.\n');
-
-    // Copy environment variables
     fs.copyFileSync(
       path.join(appPath, '.env.example'),
       path.join(appPath, '.env')
     );
     console.log('Environment files copied.\n');
-
-    // Delete .git folder
     await runCmd('npx rimraf ./.git');
-
-    // Remove unnecessary files
     fs.unlinkSync(path.join(appPath, 'CONTRIBUTING.md'));
     fs.unlinkSync(path.join(appPath, 'bin', 'create-express-lite.js'));
     fs.rmdirSync(path.join(appPath, 'bin'));
     fs.unlinkSync(path.join(appPath, 'package-lock.json'));
-
     console.log('Installation is now complete!\n');
     console.log('To start the application, navigate to the project directory:');
     console.log(`  cd ${folderName}`);
     console.log('Then run the following command:');
+    console.log('  npm install');
     console.log('  npm run dev\n');
     console.log(
       'Enjoy your production-ready Node.js app, which supports a number of ready-made features!'
@@ -120,5 +97,4 @@ async function setup() {
     console.error(`\nError: ${error.message}`);
   }
 }
-
 setup();
